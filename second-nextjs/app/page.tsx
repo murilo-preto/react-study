f6928b9034c2272357888f05f08e2c78104e33f3
Murilo Preto <murilopreto2003@outlook.com>
2026-02-05 13:52:41 -0300

    up to chapter 6

---
+++ second-nextjs/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="text-center">
+        <h1 className="text-2xl">Hello</h1>
+        <h1 className="text-2xl">Hello</h1>
+      </div>
+    </main>
+  );
+}
